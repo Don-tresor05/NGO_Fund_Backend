629d128c3fd9fa81fb4e3c0f7f46771850710d3c
Don-tresor05 <dontresor922@gmail.com>
2026-01-19 09:32:14 +0200

    allows access only to authenticate users with ADMIN role

--- accounts/permissions.py
+++ accounts/permissions.py
@@ -2,13 +2,13 @@ from rest_framework.permissions import BasePermission
 from accounts.models import User
 class IsRegistrar(BasePermission):
     """
-    Allows access only to authenticated users with a specific role.
+    Allows access only to authenticated users with ADMIN role.
     """
-    message="only regitrar can access this"
+    message="only admin can access this"
     
     def has_permission(self, request, view):
         # check authentication
-        if request.user.user_type == User.REGISTRAR :
+        if request.user.user_type == User.ADMIN :
             return True
        
         return False
